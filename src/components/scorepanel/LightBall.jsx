0d120255d0d165ca6743341a034611edea2aeffe
Yurii Komarnytskyi <kv281214@gmail.com>
2022-03-31 20:04:14 +0300

    fixed LightBall container and its wrapping

--- src/components/scorepanel/LightBall.jsx
+++ src/components/scorepanel/LightBall.jsx
@@ -3,7 +3,6 @@ import cl from './ScorePanel.module.css';
 const LightBall = (props) => {
   const rootClasses = [cl.ball];
   (props.props.name === 't')? rootClasses.push(cl.correct) : rootClasses.push(cl.incorrect);
-  console.log(rootClasses.join(' '))
   return (
     <div className={rootClasses.join(' ')}>
       {props.props.name}
