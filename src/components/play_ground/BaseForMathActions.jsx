65f688eff01f7b44a836b693a193fffdb00ad0ba
Yurii Komarnytskyi <kv281214@gmail.com>
2022-06-04 21:40:42 +0300

    Added handler for the keys that shouldnt show up in the input, plus made it possible to use Enter key in order to check users answer

--- src/components/play_ground/BaseForMathActions.jsx
+++ src/components/play_ground/BaseForMathActions.jsx
@@ -1,23 +1,29 @@
 import React, { useEffect, useRef } from 'react';
 import styles from './BaseForMathActions.module.css';
-import { createRandom } from '../../helpers';
+import { createRandom } from '../../utils';
 const BaseForMathActions = ({
   randQuest,
   setRandQuest,
   playersAnsw,
   setPlayersAnsw,
-  gameMode, }) => {
+  gameMode,
+  areAllTablesChecked,}) => {
 
+  const allowedInputKeys = ['Backspace','Delete','ArrowRight','ArrowLeft', ...Array.from(Array(9)).map((val, ind) => val = `Digit${ind}`)];
   const input_Ref = useRef(null);
-  const handleCheckButton = (input_Ref) => (!input_Ref.current.value) ? null : setRandQuest(createRandom(gameMode));
-  
+
+  const handleCheckButton = (input_Ref) => (!input_Ref.current.value) ? null : setRandQuest(createRandom(gameMode, areAllTablesChecked));
+  const handleKeyUpEvent = (e) => {
+    if(e.code === 'Enter') return handleCheckButton(input_Ref);
+    if(!allowedInputKeys.includes(e.code)) return input_Ref.current.value = ''
+  }
+   
   return (
     <div className={styles.wrapperForMathBase}>
-
       <span className={styles.generatedQuestion}> {randQuest} </span>
-
       <input
         onInput={e => setPlayersAnsw(e.target.value)}
+        onKeyUp={e => handleKeyUpEvent(e)}
         ref={input_Ref}
         value={playersAnsw}
         className={styles.enteredVal}
