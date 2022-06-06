24c304c50fb891503159f72d4106e2516662e659
Yurii Komarnytskyi <kv281214@gmail.com>
2022-06-06 14:47:50 +0300

    Fixed a bug that was causing malfunction in BaseForMathActions component, undefined would appear if all of the check boxes were uncheced, now it is impossible to uncheck them all, therefore undefined should never appear in generated question for player again

--- src/components/game_settings/GameModesCheckBoxes.jsx
+++ src/components/game_settings/GameModesCheckBoxes.jsx
@@ -8,10 +8,11 @@ const GameModesCheckBoxes = ({
   areAllTablesChecked,
   setAreAllTablesChecked, }) => {
 
-  const allTablesCheckBox = useRef(null);  
   const [boxesFromTwoToNine, setBoxesFromTwoToNine] = useState([...Array.from(Array(8)).map((val, ind) => ([val = ind + 2, false]))]);
 
   useEffect(() => {
+    console.table("BELOW IS IN USE_EFFECT");
+    console.table(boxesFromTwoToNine);
     setGameMode(boxesFromTwoToNine.reduce((acc, val) => {
         if(val[1] === true) {
         acc.push(val[0]);
@@ -21,8 +22,21 @@ const GameModesCheckBoxes = ({
     },[]))
   }, [boxesFromTwoToNine])
 
-  const handleAllTables = () => {
+  const wouldAllOfBoxesBeFalse = (arr, eventVAl) => {
+    let result = arr.reduce((acc, currArr) => {
+      if(currArr[1] === true) {
+        acc.countOfTrue += 1;
+        acc.indOfTrue = +currArr[0];
+        return acc;
+      } else return acc;
+    },{countOfTrue: 0, indOfTrue: null});
+    if(result.countOfTrue > 1 ) return false;
+    return (result.countOfTrue === 1 && +eventVAl === result.indOfTrue)? true : false;
+  }
+
+  const handleAllTables = (e) => {
     if (areAllTablesChecked) {
+      (!boxesFromTwoToNine.map((item) => item[1]).includes(true))? e.preventDefault() :
       setAreAllTablesChecked(bool => !bool); // changes areAllTablesChecked to FALSE
       setGameMode([]);
     }else {
@@ -34,6 +48,7 @@ const GameModesCheckBoxes = ({
 
   const handleSingleBox = (e) => {
     if (areAllTablesChecked) setAreAllTablesChecked(false);
+    (wouldAllOfBoxesBeFalse(boxesFromTwoToNine, e.target.value))? e.preventDefault() :
     setBoxesFromTwoToNine(prevArr => prevArr.map((item, ind) => (+item[0] === +e.target.value)? [ind+2, !item[1]] : item));
   }
 
@@ -50,10 +65,9 @@ const GameModesCheckBoxes = ({
       >
         <input
           className={styles.inputBox}
-          onChange={handleAllTables}
+          onChange={e => handleAllTables(e)}
           type='checkbox'
           checked={areAllTablesChecked}
-          ref={allTablesCheckBox}
         />
         <span>All from 2 to 9</span>
       </label>
